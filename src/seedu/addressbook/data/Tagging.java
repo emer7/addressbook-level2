86f5f339fee5ebf319c2ff526cbe1d283c0c242e
Gilbert Emerson <emer7@users.noreply.github.com>
2018-03-17 22:00:34 +0800

    Tagging.java: Add association class

---
+++ src/seedu/addressbook/data/Tagging.java
@@ -0,0 +1,8 @@
+package seedu.addressbook.data;
+
+/**
+ * Association class associating a Tag to a Person,
+ * recording a Tag that is added to or deleted from a Person.
+ */
+public class Tagging {
+}
